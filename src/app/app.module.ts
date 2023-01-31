f58ece113b5dc20cb9a59258af6e2b4a8033d5b6
Santiago Sierra Herrera <santiago.sierra@sofka.com.co>
2023-01-31 18:10:48 -0500

    chore: Initial set up and scafolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'cardsproject-90502',
+    appId: '1:843318841903:web:7773e2dc4ddbb54c14acda',
+    storageBucket: 'cardsproject-90502.appspot.com',
+    apiKey: 'AIzaSyAmgFh0T83yR231IQPqL8zvVauaRGbm9vA',
+    authDomain: 'cardsproject-90502.firebaseapp.com',
+    messagingSenderId: '843318841903',
+  },
+    production: false,
+    baseUrl: "https://localhost:7003/api/v1",
+};
